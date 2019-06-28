42173004ef69d61a848806bede843b60c4dbc0ce
Ahmedpup <45870421+Ahmedpup@users.noreply.github.com>
2019-06-28 19:29:31 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: 'NTkzNzkwMDk5OTI2Mjg2MzUy.XRTAXA.7RsUnjrGXPLoV8FzZKW__tETBh8', 
+    YT_API_KEY: 'AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4', 
+    prefix: '-',
+    devs: ['524901565472047116']
+}
